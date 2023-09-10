2202d848079f9985eb6db74e048144f00d19c27d
Ivan-Malakhovskyi <124193519+Ivan-Malakhovskyi@users.noreply.github.com>
2023-09-10 14:37:16 +0300

    add HTTP abort

--- src/components/API/apiImageService.jsx
+++ src/components/API/apiImageService.jsx
@@ -1,20 +1,25 @@
-import axios from "axios"
+import axios from 'axios';
 
-axios.defaults.baseURL = 'https://pixabay.com/api/'
-const API_KEY = '38403877-64d14ccf654d22b76dab55b11'
+axios.defaults.baseURL = 'https://pixabay.com/api/';
+const API_KEY = '38403877-64d14ccf654d22b76dab55b11';
 
- export const serviceGallery = async (query,page) => {
-    const params = new URLSearchParams({
-        key: API_KEY,
-        q: query,
-        image_type: 'photo',
-        orientation: 'horizontal',
-         safesearch: true,
-        per_page: 12,
-        page: page
-})
-     
-     const { data } = await axios.get(`?${params}`)   
-     return data
-     
-}
+export const serviceGallery = async (query, page, controller) => {
+  const params = new URLSearchParams({
+    key: API_KEY,
+    q: query,
+    image_type: 'photo',
+    orientation: 'horizontal',
+    safesearch: true,
+    per_page: 12,
+    page: page,
+  });
+
+  //   try {
+  const { data } = await axios.get(`?${params}`, {
+    signal: controller.current.signal,
+  });
+  return data;
+  //   } catch (error) {
+  //     console.log(error.message);
+  //   }
+};
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,136 +1,147 @@
-import { useState,useEffect } from "react";
+import { useState, useEffect, useRef } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
 import { animateScroll as scroll } from 'react-scroll';
-import { Loader } from "./loader/loader";
-import { GlobalStyle } from "./GlobalStyle";
-import { ImageGallery } from "./imageGallery/imageGallery";
-import { serviceGallery } from "./API/apiImageService";
-import { Searchbar } from "./searchBar/searchBar";
-import { ErrorMsg } from "./loader/loader.styled";
-import { Button } from "./button/button";
-import { BtnToTop } from "./button/bytton.styled";
-
+import { Loader } from './loader/loader';
+import { GlobalStyle } from './GlobalStyle';
+import { ImageGallery } from './imageGallery/imageGallery';
+import { serviceGallery } from './API/apiImageService';
+import { Searchbar } from './searchBar/searchBar';
+import { ErrorMsg } from './loader/loader.styled';
+import { Button } from './button/button';
+import { BtnToTop } from './button/bytton.styled';
 
 export const App = () => {
-
-  const [query, setQuery] = useState('')
-  const [page,setPage] = useState(1) 
-  const [error,setError] = useState(false)
-  const [loading,setLoading] = useState(false)
-  const [initialQuery, setInitialQuery] = useState('')
-  const [images,setImages] = useState([])
-  const [showLoadMoreButton, setShowLoadMoreButton] = useState(true)
-  const [searchFailed, setSearchFailed] = useState(false)
-
+  const [query, setQuery] = useState('');
+  const [page, setPage] = useState(1);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [initialQuery, setInitialQuery] = useState('');
+  const [images, setImages] = useState([]);
+  const [showLoadMoreButton, setShowLoadMoreButton] = useState(true);
+  const [searchFailed, setSearchFailed] = useState(false);
 
   const scrollToTop = () => {
-    scroll.scrollToTop()
-  }
+    scroll.scrollToTop();
+  };
 
-  
+  const controller = useRef();
+
   useEffect(() => {
     if (!query) {
-      return
+      return;
     }
-    
-    const fetchImages = async () => {
-      
-      try { 
-        setLoading(true)
-        setError(false)
-        
 
-      const { hits, totalHits } = await serviceGallery(query, page)
-      
-      const filteredNeedsValues = () => {
-         
-           setImages( prevState => (
-           [...prevState, ...hits.map((image) => ({
+    const fetchImages = async () => {
+      //* Якщо є попередній запит -> відмінити
+
+      if (controller.current) {
+        controller.current.abort();
+      }
+
+      //*Створити новий контроллер
+      controller.current = new AbortController();
+
+      try {
+        setLoading(true);
+        setError(false);
+
+        const { hits, totalHits } = await serviceGallery(
+          query,
+          page,
+          controller
+        );
+
+        const filteredNeedsValues = () => {
+          setImages(prevState => [
+            ...prevState,
+            ...hits.map(image => ({
               id: image.id,
               largeImageURL: image.largeImageURL,
               webformatURL: image.webformatURL,
-              tags: image.tags,  
-            }))]
-           ))
-      }
-      
-      setShowLoadMoreButton(false)
-      
-        
+              tags: image.tags,
+            })),
+          ]);
+        };
+
+        setShowLoadMoreButton(false);
+
         if (page === Math.ceil(totalHits / 12)) {
-          toast.success('You have reached the end of the list of images found')
-         filteredNeedsValues()
-          return
+          toast.success('You have reached the end of the list of images found');
+          filteredNeedsValues();
+          return;
         }
 
-      if (hits.length === 0) {
-          setSearchFailed(true)
-      }   
-       
-     
-        filteredNeedsValues()
-        setShowLoadMoreButton(true)
-      
+        if (hits.length === 0) {
+          setSearchFailed(true);
+        }
 
+        filteredNeedsValues();
+        setShowLoadMoreButton(true);
+      } catch (error) {
+        // console.log(error.message);
+        if (error.code !== 'ERR_CANCELED') {
+          setError(true);
+        }
 
+        setError(true);
+      } finally {
+        setLoading(false);
+      }
+    };
 
-    } catch (error) {
+    fetchImages();
+  }, [query, page]);
 
-      console.log(error)
+  const handleSubmit = query => {
+    setInitialQuery(initialQuery);
+    setQuery(query);
+    setImages([]);
+    setPage(1);
+    setError(false);
+  };
 
-      setError(true)
-    } finally {
-        setLoading(false)
-    }
-  }
-
-  fetchImages()
-
-},[query,page])
-
-  
-  const handleSubmit = (query) => {
-    setInitialQuery(initialQuery) 
-    setQuery(query)
-    setImages([])
-    setPage(1)
-    setError(false) 
-  }
-  
-  
   const handleLoadMore = () => {
+    setPage(prevPage => prevPage + 1);
 
-    setPage(prevPage => prevPage + 1)
-    
- 
-     scroll.scrollToBottom(260 * 2, {
-       duration: 250,
-       smooth: 'easeInOutQuint',
-    })
-  }
-
-
+    scroll.scrollToBottom(260 * 2, {
+      duration: 250,
+      smooth: 'easeInOutQuint',
+    });
+  };
 
   return (
     <div>
-      <Searchbar onSubmit={handleSubmit}  /> 
+      <Searchbar onSubmit={handleSubmit} />
       {loading && <Loader />}
-      
-      {images.length > 0 && (<ImageGallery hits={images} />)}
-      
-      {page === 41 && (toast.success('You have reached the last page'))}
 
-      { searchFailed && images.length === 0 && !loading && (
-        <ErrorMsg>Such images was not found, try find something else 😉</ErrorMsg>)}
-      
-      {error && !loading && <ErrorMsg>❌ Something went wrong,try reload page {toast.error('Ooops, something went wrong')}
-        </ErrorMsg>}
+      {images.length > 0 && <ImageGallery hits={images} />}
 
-      {images.length > 0 && showLoadMoreButton && !loading && (<Button loadMore={ handleLoadMore}/>)}
-      <Toaster/>
+      {page === 41 && toast.success('You have reached the last page')}
+
+      {searchFailed && images.length === 0 && !loading && (
+        <ErrorMsg>
+          Such images was not found, try find something else 😉
+        </ErrorMsg>
+      )}
+
+      {error && !loading && !controller && (
+        <ErrorMsg>
+          ❌ Something went wrong,try reload page{' '}
+          {toast.error('Ooops, something went wrong')}
+        </ErrorMsg>
+      )}
+
+      {images.length > 0 && showLoadMoreButton && !loading && (
+        <Button loadMore={handleLoadMore} />
+      )}
+      <Toaster />
       <GlobalStyle />
 
-      {images.length > 0 && <BtnToTop type="button" onClick={scrollToTop}>To start</BtnToTop>}
+      {images.length > 0 && (
+        <BtnToTop type="button" onClick={scrollToTop}>
+          To start
+        </BtnToTop>
+      )}
     </div>
   );
 };
